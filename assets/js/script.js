3b65d4f6003be026735abac9451a7a0b63c175e5
Thiago <thiagohdosanjos99@gmail.com>
2021-01-16 21:36:57 -0300

    Landin Page Sambatuca projeto 1 atualização 1

---
+++ assets/js/script.js
@@ -0,0 +1,58 @@
+let nome = window.document.getElementById('nome')
+let email = document.querySelector('#email')
+let assunto = document.querySelector('#assunto')
+let nomeOk = false
+let emailOk = false
+let assuntoOk = false
+let mapa = document.querySelector('#mapa')
+
+nome.style.width = '100%'
+email.style.width = '100%'
+
+function validaNome() {
+
+   let txtNome = document.querySelector('#txtNome')
+   if (nome.value.length < 3) {
+      txtNome.innerHTML = 'Nome Inválido'
+      txtNome.style.color = 'red' 
+   } else {
+      txtNome.innerHTML = '✔'
+      txtNome.style.color = 'green'
+      nomeOk = true
+   }
+
+}
+
+function validaEmail() {
+   let txtEmail = document.querySelector('#txtEmail') 
+
+   if (email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1 ) {
+      txtEmail.innerHTML = 'E-mail inválido'
+      txtEmail.style.color = 'red'
+   } else {
+      txtEmail.innerHTML = '✔'
+      txtEmail.style.color = 'green'
+      emailOk = true
+   }
+}
+
+function validaAssunto() {
+   let txtAssunto = document.querySelector('#txtAssunto')
+
+   if (assunto.value.length >= 101) {
+      txtAssunto.innerHTML = 'Texto é muito grande, digite no máximo 100 caracteres'
+      txtAssunto.style.color = 'red'
+      txtAssunto.style.display = 'block'
+   } else {
+      txtAssunto.style.display = 'none'
+      assuntoOk = true
+   }
+}
+
+function enviar() {
+   if (nomeOk == true && emailOk == true && assuntoOk == true) {
+      alert ('Formulário enviado com sucesso!')
+   } else {
+      alert ('Preencha o formulário corretamente antes de enviar...')
+   }
+}
